023507dbe78a2e054816963fe463d6821363c381
Martin Dunschen <martin.dunschen@autodesk.com>
2017-12-06 09:56:30 +0000

    Added tweepy api calls, added location code to get streetname and postcode.

---
+++ locService.py
@@ -0,0 +1,25 @@
+import urllib
+import urllib.request as request
+import json
+
+def getNearestStreet(lat, lon):
+    urlstr = "http://router.project-osrm.org/nearest/v1/driving/%s,%s" % (lon, lat)
+    r = request.urlopen(urlstr).read()
+    cont = json.loads(r.decode('utf-8'))
+    if cont['code'] == "Ok":
+        return cont["waypoints"][0]["name"]
+
+
+def getPostCode(lat, lon):
+    urlstr = "http://api.postcodes.io/postcodes?lat=%s&lon=%s" % (lat, lon)
+    r = request.urlopen(urlstr).read()
+    cont = json.loads(r.decode('utf-8'))
+    print (cont["status"])
+    if cont["status"] and cont["result"]:
+        return cont["result"][-1]["outcode"]
+
+
+if __name__ == "__main__":
+    print(getNearestStreet("53.4025", "-2.9865"))
+    print(getPostCode("53.4025", "-2.9865"))
+
