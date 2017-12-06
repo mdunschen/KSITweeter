f51f7eaf5ea1e923db42cb0c9788798a9024a8fd
Martin Dunschen <martin.dunschen@autodesk.com>
2017-12-06 09:59:20 +0000

    work on tweepy api calls, added location information

--- ksitweetbot.py
+++ ksitweetbot.py
@@ -1,6 +1,12 @@
 import datetime
 import re
 import sys
+import pickle
+import operator
+sys.path.append("C:\\Program Files\\Anaconda3\\envs\\tensorflow\\lib\\site-packages")
+import tweepy
+
+from locService import getNearestStreet, getPostCode
 
 vehicle_type = {
     1:("Pedal cycle","bicycle"),\
@@ -47,6 +53,46 @@ translations = {"2  (Female)": "woman",
                 "-1":"a"}
 
 
+consumer_key, consumer_secret, access_token, access_token_secret = pickle.load(open("apikeys.bin", "rb")) 
+
+def twitterAPI():
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth)
+    return api
+
+
+
+def tweet(status, replyto=None, imgfilename=None):
+    if not (status or imgfilename):
+        return
+    urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', status)
+    print("urls = ", urls)
+    # take out all url texts from status for count, all urls count as 23
+    rstat = status
+    for u in urls:
+        rstat = rstat.replace(u, '')
+    nchars = len(rstat) + 23 * len(urls)
+    if nchars > 140:
+        print("Tweet too long")
+        
+    #print(status)
+    
+    api = twitterAPI()
+    if (imgfilename and os.path.isfile(imgfilename)):
+        try:
+            stat = api.update_with_media(imgfilename, status=status, in_reply_to_status_id=(replyto and replyto.id))
+        except Exception as e:
+            print(e)
+            stat = None
+        
+    else:
+        try:
+            stat = api.update_status(status=status, in_reply_to_status_id=(replyto and replyto.id))
+        except Exception as e:
+            print(e)
+            stat = None
+    return stat
 def casualtyDesc(gender, age_band):
     if age_band in ("01  (0 - 5)", "02  (6 - 10)", "03  (11 - 15)"):
         return "a child"
@@ -88,7 +134,7 @@ def sortByDateTime(ksiData):
         # make a datetime object
         dt = datetime.datetime.strptime("%s %s" % (date, tim), "%d/%m/%Y %H:%M")
         sortedksiData.append((dt, r))
-    sortedksiData.sort()
+    sortedksiData.sort(key=operator.itemgetter(0))
     return sortedksiData
 
 def translate(phrase):
@@ -101,7 +147,23 @@ def composeTweet(eventDate, record):
     tweetContent = []
     cd = casualtyDesc(record["Sex_of_Casualty"], record["Age_Band_of_Casualty"])
     pronoun = translate(cd.split(" ")[-1])
-    tweetContent.append("Today, at %s, %d year(s) ago, %s was seriously injured when a %s collided into %s." % (record['Time'], datetime.datetime.now().year - eventDate.year, cd, vehicle_type[record["Vehicle_Type"][0]][1], pronoun)) 
+    lat, lon = record["Latitude"], record["Longitude"]
+    streetname = getNearestStreet(lat, lon)
+    if streetname:
+        pc = getPostCode(lat, lon)
+        if pc:
+            streetinfo = " on %s (%s)" % (streetname, pc)
+        else:
+            streetinfo = " on %s" % (streetname)
+    else:
+        streetinfo = ""
+    tweetContent.append("Today, at %s, %d year(s) ago%s, %s was seriously injured when a %s collided into %s." % (\
+            record['Time'], \
+            datetime.datetime.now().year - eventDate.year, \
+            streetinfo, \
+            cd, \
+            vehicle_type[record["Vehicle_Type"][0]][1], \
+            pronoun)) 
     return ''.join(tweetContent)
         
 
@@ -109,10 +171,12 @@ def composeTweet(eventDate, record):
 
 if __name__ == "__main__":
     ksiData = readKSIData("2016_05.tsv")
-    addVehicleData(ksiData, r"dftRoadSafetyData_Vehicles_2016\veh.csv")
+    addVehicleData(ksiData, r"Veh.csv")
     sortedksiData = sortByDateTime(ksiData)
     for i, de in enumerate(sortedksiData):
         date, event = de
         now = datetime.datetime.now()
-        print now - date
-        print i, composeTweet(date, event)
+        print (date)
+        if date.month == now.month and date.day == now.day:
+            print(now - date)
+            print(i, composeTweet(date, event))
