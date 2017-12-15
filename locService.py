8e8900a554a9646a5c88468369d254cc48dc5db7
Martin Dunschen <martin.dunschen@autodesk.com>
2017-12-15 17:30:34 +0000

    Added getting ward name from lat/lon

--- locService.py
+++ locService.py
@@ -18,8 +18,20 @@ def getPostCode(lat, lon):
     if cont["status"] and cont["result"]:
         return cont["result"][-1]["outcode"]
 
+def getWard(lat, lon):
+    urlstr = "http://api.postcodes.io/postcodes?lat=%s&lon=%s" % (lat, lon)
+    r = request.urlopen(urlstr).read()
+    cont = json.loads(r.decode('utf-8'))
+    print (cont["status"])
+    if cont["status"] and cont["result"]:
+        return cont["result"][-1]["admin_ward"]
+
 
 if __name__ == "__main__":
     print(getNearestStreet("53.4025", "-2.9865"))
     print(getPostCode("53.4025", "-2.9865"))
+    print(getWard("53.4025", "-2.9865"))
+    print(getNearestStreet("53.372420", "-2.861337"))
+    print(getPostCode("53.372420", "-2.861337"))
+    print(getWard("53.372420", "-2.861337"))
 
